672a8f23edb986fd39904ef14a85e9063c2f40fb
Luiz Eduardo <eduardolecdt@gmail.com>
2021-04-27 18:13:09 -0300

    JS modal videos

---
+++ assets/js/pages/index/sectionVideo.js
@@ -0,0 +1,32 @@
+(function (){
+
+  // VARIABLES
+
+  const section = document.querySelector('section[name="video"]')
+  const img = section.querySelector('img[name="previa"]')
+  const modal = section.querySelector('div[name="modal"]')
+  const button = section.querySelector('button[name="fechar"]')
+  const iframe = section.querySelector('iframe')
+
+  // FUNCTIONS
+  
+  function abrirModal () {
+    img.addEventListener('click', () => {
+      modal.classList.add('mostrar')
+      iframe.src = 'https://www.youtube-nocookie.com/embed/-_6_T-mvPog'
+    })
+  }
+
+  function fecharModal () {
+    button.addEventListener('click', () => {
+      modal.classList.remove('mostrar')
+      iframe.src = ''
+    })
+  }
+
+  // ATIVAR TUDO
+
+  abrirModal()
+  fecharModal()
+
+}())
