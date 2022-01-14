91f3c9811d956929820df1e1558995f1163bff22
Eduardo Lecdt <eduardolecdt@gmail.com>
2022-01-14 14:37:28 -0300

    Ajuste texto

--- assets/js/pages/index/sectionVideo.js
+++ assets/js/pages/index/sectionVideo.js
@@ -27,31 +27,4 @@
   abrirModal1()
   fecharModal1()
 
-  // Vídeo 2
-
-  const section2 = document.querySelector('section[name="video2"]')
-  const img2 = section2.querySelector('img[name="previa"]')
-  const modal2 = section2.querySelector('div[name="modal"]')
-  const button2 = section2.querySelector('button[name="fechar"]')
-  const iframe2 = section2.querySelector('iframe')
-
-  // FUNCTIONS
-  
-  function abrirModal2 () {
-    img2.addEventListener('click', () => {
-      modal2.classList.add('mostrar')
-      iframe2.src = 'https://www.youtube.com/embed/-_6_T-mvPog'
-    })
-  }
-
-  function fecharModal2 () {
-    button2.addEventListener('click', () => {
-      modal2.classList.remove('mostrar')
-      iframe2.src = ''
-    })
-  }
-
-  abrirModal2()
-  fecharModal2()
-
 }())
